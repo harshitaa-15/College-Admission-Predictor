9553327eddd43a17b8bb5cf173f31248a8026a16
Harshita Gupta <jai.mahakal156213@gmail.com>
2026-05-17 09:53:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ st.set_page_config(
 #===================================================================================================================
 
 st.image("college.webp")
-
 st.title("🎓College Adimission Prediction")
 st.write("Hey!🖐 Here you can get the probability of your admission in the college, by giving a small infomation.")
 st.markdown("---")
