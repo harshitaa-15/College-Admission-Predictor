12817bf62ef22dc64f33d6f5c3ec281e8434a028
Harshita Gupta <jai.mahakal156213@gmail.com>
2026-07-02 11:12:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 
 #==================================================================================================================
 
-
 st.set_page_config(
     page_title = "College Adimission Prediction! ",
     page_icon = "🎓"
